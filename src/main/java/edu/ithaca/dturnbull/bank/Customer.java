92f274ee830831bf24185cdcd53b0b484cb258cf
macmahand <61165339+macmahand@users.noreply.github.com>
2022-02-11 11:29:23 -0500

    Customer class implemented

---
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+
+    String name;   
+    String customerID;
+    String password;
+    Account[] accounts;
+
+    public Customer(String nameIn, String customerID, String password) {
+
+    }
+
+    public void addAccount(Account newAccount) {
+        
+    }
+    
+}
