1ff2a2920128d999a84415fe6f322b4da2733869
macmahand <61165339+macmahand@users.noreply.github.com>
2022-02-27 20:26:00 -0500

    added get() methods for Customer private variables

--- src/main/java/edu/ithaca/dturnbull/bank/Customer.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer.java
@@ -23,6 +23,18 @@ public class Customer {
         accounts.remove(accountToDelete);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public ArrayList<Account> getAccounts() {
         return this.accounts;
     }
